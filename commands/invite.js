a8093d15acac0a51f4fa2513d08cd534e8c2dade
Bumi <bumi.iii001@gmail.com>
2021-05-16 22:05:38 -0400

    small update to invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new discord.MessageEmbed()
         .setColor('#039154')
         .setAuthor('Bumi', 'https://i.imgur.com/OaqryKu.png', "https://account.venmo.com/u/Bumiii")
-        .setTitle("The link below is the link to the Bumi's Bots Support Server")
+        .setTitle("The link below is the link to Bumi's Bots Support Server")
         .addFields(
             { name: 'Link to invite BumiBeats', value: 'https://discord.com/oauth2/authorize?client_id=821474236698853427&scope=bot&permissions=8', inline: true },
             { name: "Link to the Bumi's Bots support server", value: 'https://discord.gg/jQ9Q4SJXXn', inline: true },
